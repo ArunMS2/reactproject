f7056b247dee4e39ce3700ead668be8db4c74931
Arun MS <arunmsprofessional@gmail.com>
2022-12-10 15:07:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  return (
+    <div className="App">
+      {
+      
+      }
+      </div>
+    
+  );
+}
+export default App;
